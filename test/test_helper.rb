b75fcb107aef140a78e3f3776b0f5c27b6e928fa
Michael Lewandowski <milewgit@gmail.com>
2014-11-25 10:53:08 -0500

    Eat warning messages emitted when requir'ing coveralls

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,13 @@
-require 'coveralls'           # must be before everything else
-Coveralls.wear!
+# Eat warning messages emitted when coveralls loaded.
+# See http://devblog.avdi.org/2011/08/25/temporarily-disabling-warnings-in-ruby/
+begin
+  require 'stringio'
+  $stderr, original_stderr = StringIO.new, $stderr
+  require 'coveralls'         # must be before everything else
+  Coveralls.wear!
+ensure
+  $stderr = original_stderr
+end
 
 require 'minitest/autorun'
 require 'mocha/setup'         # must be after require 'minitest/autorun'
